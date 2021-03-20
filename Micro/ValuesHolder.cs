65a608fd44b3b538a4a04c79f9810159e86a90ec
estepanova <stepanova_eg@bk.ru>
2021-03-20 23:00:42 +0300

    HW to lesson 1 Microservices

---
+++ Micro/ValuesHolder.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Micro 
+{
+    public class ValuesHolder
+    {
+        public List<WeatherForecast> Values {get; set;} = new List<WeatherForecast>();
+    }
+}
