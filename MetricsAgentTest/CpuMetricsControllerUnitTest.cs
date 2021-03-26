821cc805089f158f02a2a736c5262e47cb9db702
estepanova <stepanova_eg@bk.ru>
2021-03-26 11:45:12 +0300

    Небольшое изменение

--- MetricsAgentTest/CpuMetricsControllerUnitTest.cs
+++ MetricsAgentTest/CpuMetricsControllerUnitTest.cs
@@ -16,15 +16,12 @@ namespace MetricsAgentTest
 
         [Fact]
         public void GetMetrics_ReturnsOk()
-        {
-            //Arrange
+        {            
             var fromTime = TimeSpan.FromSeconds(0);
             var toTime = TimeSpan.FromSeconds(100);
-
-            //Act
+           
             var result = controller.GetMetrics(fromTime, toTime);
-
-            // Assert
+            
             _ = Assert.IsAssignableFrom<IActionResult>(result);
         }
 
